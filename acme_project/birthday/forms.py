83ba617ef134ed351827b28e6252f7f55955279b
Красиков Дмитрий <z1r0-d@yandex.ru>
2023-05-03 18:47:11 +0300

    Спринт 7/25 → Тема 2/8: Работа с формами → Урок 7/10

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 
 
-class BirthdayForm(forms.Form):
-    first_name = forms.CharField(label='Имя', max_length=20)
-    last_name = forms.CharField(
-        label='Фамилия', required=False, help_text='Необязательное поле'
-    )
-    birthday = forms.DateField(
-        label='Дата рождения',
-        widget=forms.DateInput(attrs={'type': 'date'})
-    )
+from .models import Birthday
+
+class BirthdayForm(forms.ModelForm):
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Birthday
+        # Указываем, что надо отобразить все поля.
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'})
+        } 
