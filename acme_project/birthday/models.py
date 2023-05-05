d493b60ea75fa6b4872d5010ca67fa4c35b1c9ba
Красиков Дмитрий <z1r0-d@yandex.ru>
2023-05-05 15:26:31 +0300

    Validate&Clean

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -2,9 +2,25 @@
 from django.db import models
 
 
+from .validators import real_age
+
+
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
         'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
     )
-    birthday = models.DateField('Дата рождения') 
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+
+    class Meta:
+        verbose_name = 'День рождения'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} - {self.birthday}'
